1942a79d8aab397b650586b3ecc528ac366dcf9e
DiyankaGovender <diyankag@gmail.com>
2025-09-21 13:14:28 +0200

    Main menu added

---
+++ Assets/Scripts/MainMenuAnim.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuAnim : MonoBehaviour
+{
+    [SerializeField] public MeshRenderer mesh;
+    [SerializeField]  public Material material;
+
+    [SerializeField] public CanvasRenderer canvasRenderer;
+
+
+    void Start()
+    {
+
+    }
+
+
+}
